8931e8f980987ed3056f61b0cfe890ffc4a24bfc
ole-moller <140734002+ole-moller@users.noreply.github.com>
2026-04-20 19:54:18 +0200

    Rename module to double_dabble_SV

--- src/double_dabble_SV.v
+++ src/double_dabble_SV.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_ole_moller_double_dabble_SV (
+module double_dabble_SV (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
